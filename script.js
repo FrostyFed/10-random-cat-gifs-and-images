71200053d6a4e61c548d226f9b943949a38c7533
FrostyFed <mdartsasvili@gmail.com>
2024-10-11 09:30:01 +0300

    10 random cat gifs and images
    changed the website from random games to random cat pics and gifs due to the game api not working

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const url = "https://www.freetogame.com/api/games?platform=pc"
+document.addEventListener('DOMContentLoaded', () => {
+    const url = "https://api.thecatapi.com/v1/images/search?limit=10";
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const container = document.querySelector('.container');
+
+            container.innerHTML = ''; 
+
+            data.forEach(cat => {
+                const gameContainer = document.createElement('div');
+                gameContainer.classList.add('game');
+                
+                const img = document.createElement('img');
+                img.src = cat.url;
+                img.alt = "Random Cat";
+                img.style.width = '100%';
+                img.style.height = 'auto';
+
+                gameContainer.appendChild(img);
+
+                container.appendChild(gameContainer);
+            });
+        })
+});
